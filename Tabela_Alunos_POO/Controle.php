1dca11ec7614d58d27a2a2f2cb36603998e3fdfb
Lucas Siqueira <89342760+LucasSiqueiraNaN@users.noreply.github.com>
2022-05-23 20:08:15 -0300

    Add files via upload

---
+++ Tabela_Alunos_POO/Controle.php
@@ -0,0 +1,24 @@
+<?php
+  include "Aluno.php";
+ 
+  $alunos =  array();
+ 
+  for($i=0;$i<10;$i++){
+      $nome = $_GET['NOME'.($i+1)];
+      $nota = $_GET['NOTA'.($i+1)];
+ 
+      $alunos[$i] = new Aluno();
+      $alunos[$i]->setNome($nome);
+      $alunos[$i]->setNota($nota);
+  }
+ 
+  $outroAluno = new Aluno();
+ 
+  $media = $outroAluno->Media($alunos);
+  $NomeMaior = $outroAluno->MaiorNota($alunos);
+  $tabela = $outroAluno->Tabela($alunos);
+ 
+  echo "Media: <b>".$media."</b><br>";
+  echo "Nome do aluno com a maior nota: <b>".$NomeMaior."</b><br>";
+  echo "<b>".$tabela."</b><br>";
+?>
